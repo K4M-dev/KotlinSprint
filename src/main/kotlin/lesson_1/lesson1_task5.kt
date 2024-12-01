ce68f33996a70875b02bf93643ddd424b52a0309
AlexKir <yashik.alex2951@mail.ru>
2024-12-01 19:18:13 +0300

    KS-1-5 refactor const 3.0

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,14 @@
 package org.example.lesson_1
 
-const val divider = 60
+const val DIVIDER_FOR_TIME = 60
 
 fun main() {
 
     val seconds = 6480
-    val minute = seconds / divider
-    val hour = minute / divider
-    val remainingSeconds = seconds % divider
-    val remainingMinutes = minute % divider
+    val minute = seconds / DIVIDER_FOR_TIME
+    val hour = minute / DIVIDER_FOR_TIME
+    val remainingSeconds = seconds % DIVIDER_FOR_TIME
+    val remainingMinutes = minute % DIVIDER_FOR_TIME
 
     print("%02d:%02d:%02d".format(hour, remainingMinutes, remainingSeconds))
 }
