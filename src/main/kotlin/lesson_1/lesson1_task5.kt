a0c6b1e4a2c501df14b9eeff1748a6f2a1ab949b
AlexKir <yashik.alex2951@mail.ru>
2024-12-01 18:08:24 +0300

    KS-1-5 refactor const

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,14 +1,15 @@
 package org.example.lesson_1
 
-const val x = 60
+const val secondsInMinute = 60
+const val minutsInHour = 60
 
 fun main() {
 
     val seconds = 6480
-    val minute = seconds / x
-    val hour = minute / x
-    val remainingSeconds = seconds % x
-    val remainingMinutes = minute % x
+    val minute = seconds / secondsInMinute
+    val hour = minute / minutsInHour
+    val remainingSeconds = seconds % secondsInMinute
+    val remainingMinutes = minute % minutsInHour
 
     print("%02d:%02d:%02d".format(hour, remainingMinutes, remainingSeconds))
 }
