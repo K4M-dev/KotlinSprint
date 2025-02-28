4ed7eb058500f5790d3a50304bacc83feafdae1b
AlexKir <yashik.alex2951@mail.ru>
2025-03-01 00:29:32 +0300

    KS-5-3 refactor cycles

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -3,22 +3,18 @@ package org.example.lesson_5
 fun main() {
     println("Введите первое число от 0 до 42")
 
-    val a = readln().toInt()
+    val firstNumber = readln().toInt()
 
     println("Введите второе число от 0 до 42")
 
-    val b = readln().toInt()
+    val secondNumber = readln().toInt()
 
     val winNumberFirst = 17
     val winNumberSec = 19
 
-    if ((a == winNumberFirst) && (b == winNumberSec)) {
+    if (((firstNumber == winNumberFirst) && (secondNumber == winNumberSec)) || ((firstNumber == winNumberSec) && (secondNumber == winNumberFirst))) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if ((a == winNumberSec) && (b == winNumberFirst)) {
-        println("Поздравляем! Вы выиграли главный приз!")
-    } else if ((a == winNumberFirst) || (b == winNumberFirst)) {
-        println("Вы выиграли утешительный приз!")
-    } else if ((a == winNumberSec) || (b == winNumberSec)) {
+    } else if (((firstNumber == winNumberFirst) || (secondNumber == winNumberFirst)) || ((firstNumber == winNumberSec) || (secondNumber == winNumberSec))) {
         println("Вы выиграли утешительный приз!")
     } else {
         println("Неудача")
