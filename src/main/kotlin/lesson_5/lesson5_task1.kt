d4bc0e898284f91e11884ec9aaf490ee41f0ee1e
AlexKir <yashik.alex2951@mail.ru>
2025-03-01 00:33:53 +0300

    KS-5-1 refactor variables

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -3,9 +3,9 @@ package org.example.lesson_5
 fun main() {
     println("Сложите два числа: 3 и 7")
 
-    val c = readln().toInt()
+    val userNumber = readln().toInt()
 
-    if (c == 10) {
+    if (userNumber == 3 + 7) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещён")
