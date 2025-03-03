ddef8ce1a3762868eb90aa13c8e469d032df0136
AlexKir <yashik.alex2951@mail.ru>
2025-03-03 23:18:04 +0300

    KS-5-1 refactor strings

--- src/main/kotlin/lesson_5/lesson5_task3.kt
+++ src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -12,9 +12,11 @@ fun main() {
     val winNumberFirst = 17
     val winNumberSec = 19
 
-    if (((firstNumber == winNumberFirst) && (secondNumber == winNumberSec)) || ((firstNumber == winNumberSec) && (secondNumber == winNumberFirst))) {
+    if (((firstNumber == winNumberFirst) && (secondNumber == winNumberSec))
+        || ((firstNumber == winNumberSec) && (secondNumber == winNumberFirst))) {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if (((firstNumber == winNumberFirst) || (secondNumber == winNumberFirst)) || ((firstNumber == winNumberSec) || (secondNumber == winNumberSec))) {
+    } else if (((firstNumber == winNumberFirst) || (secondNumber == winNumberFirst))
+        || ((firstNumber == winNumberSec) || (secondNumber == winNumberSec))) {
         println("Вы выиграли утешительный приз!")
     } else {
         println("Неудача")
