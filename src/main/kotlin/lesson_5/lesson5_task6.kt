714bc7a49e30bcad31585ebe65af847e1ac28525
ALEXKIR <yashik.alex2951@mail.ru>
2025-03-06 21:26:38 +0300

    KS-5-6 program for body mass index

---
+++ src/main/kotlin/lesson_5/lesson5_task6.kt
@@ -0,0 +1,33 @@
+package org.example.lesson_5
+
+import kotlin.math.pow
+
+fun main() {
+    println("Введите массу своего тела")
+
+    val weight = readln().toDouble()
+
+    println("Введите свой рост")
+
+    val height = readln().toDouble()
+
+    val heightInMeters = height / FOR_METERS
+
+    val imt: Double = weight / (heightInMeters).pow(2)
+
+    val categoryValid = when {
+        imt < MIN_IMT -> "Недостаточная масса тела"
+        imt in MIN_IMT..NORMAL_IMT -> "Нормальная масса тела"
+        imt in NORMAL_IMT..MAX_IMT -> "Избыточная масса тела"
+        else -> "Ожирение"
+    }
+
+    println("Ваш индекс массы тела: ${"%.2f".format(imt)}")
+    println("Ваша категория ИМТ: $categoryValid")
+
+}
+
+const val FOR_METERS = 100
+const val MIN_IMT: Double = 18.50
+const val NORMAL_IMT: Double = 25.00
+const val MAX_IMT: Double = 30.00
