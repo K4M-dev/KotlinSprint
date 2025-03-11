89b1e7bc228b6bc2f76c97ba509b14c93d954bb0
ALEXKIR <yashik.alex2951@mail.ru>
2025-03-11 19:38:05 +0300

    KS-6-5 create check for bot

---
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -0,0 +1,25 @@
+package org.example.lesson_6
+
+import kotlin.random.Random
+
+fun main() {
+
+    var attempts = 3
+
+    do {
+        val firstNumber = Random.nextInt(1, 10)
+        val secondNumber = Random.nextInt(1, 10)
+        println("Сложите $firstNumber и $secondNumber")
+        val userNumber = readln().toInt()
+        if (userNumber == secondNumber + firstNumber) {
+            println("Добро пожаловать!")
+            return
+        } else (println("Неверно! Осталось попыток: ${--attempts}"))
+    } while (attempts > ZERO_ATTEMPTS)
+
+    println("Доступ запрещен")
+}
+
+const val ZERO_ATTEMPTS = 0
+
+
