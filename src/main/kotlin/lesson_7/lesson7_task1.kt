4f257d96ecbf7a91c56e59e353dc2cce65883f26
ALEXKIR <yashik.alex2951@mail.ru>
2025-03-16 15:27:55 +0300

    KS-7-1 refactor generate pass

--- src/main/kotlin/lesson_7/lesson7_task1.kt
+++ src/main/kotlin/lesson_7/lesson7_task1.kt
@@ -2,12 +2,18 @@ package org.example.lesson_7
 
 fun main() {
 
-    val charPool = ('a'..'z') + (0..9)
+    val charPool1 = ('a'..'z')
+    val charPool2 = (0..9)
     val password = StringBuilder()
 
     for (i in 6 downTo 1) {
-        val randomChar = charPool.random()
-        password.append(randomChar)
+        if (i % 2 == 0) {
+            val randomChar1 = charPool1.random()
+            password.append(randomChar1)
+        } else {
+            val randomChar2 = charPool2.random()
+            password.append(randomChar2)
+        }
     }
 
     println(password)
