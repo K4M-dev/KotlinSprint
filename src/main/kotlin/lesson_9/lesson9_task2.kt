b30e63eace916dbbba1bac9a0656171df60639d5
AlexKir <yashik.alex2951@mail.ru>
2025-03-25 19:07:41 +0300

    KS-9-2 base list with function add new ingredients

---
+++ src/main/kotlin/lesson_9/lesson9_task2.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_9
+
+fun main() {
+
+    val baseIngredients = mutableListOf("Яйцо", "Перец", "Лук")
+
+    println("В рецепте есть следующие ингредиенты: ${baseIngredients.joinToString(", ")}")
+
+    println("Желаете ли добавить ещё?")
+
+    val answer = readln().toString()
+
+    if (POSITIVE_RESPONSE.equals(answer, ignoreCase = true)) {
+        println("Напишите ингридиент, который хотите добавить")
+        baseIngredients.add(readln().toString())
+        println("Теперь в рецепте есть следующие ингредиенты: ${baseIngredients.joinToString(", ")}")
+    } else (return)
+
+}
+
+const val POSITIVE_RESPONSE = "Да"
