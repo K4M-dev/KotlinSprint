8c87420c49047226e98990508ef1ffab97dfb7ee
ALEXKIR <yashik.alex2951@mail.ru>
2025-03-31 16:07:45 +0300

    KS-10-3 generate pass with user length

---
+++ src/main/kotlin/lesson_10/lesson10_task3.kt
@@ -0,0 +1,21 @@
+package org.example.lesson_10
+
+fun main() {
+    println("Введите длину пароля")
+    val passLength = readln().toInt()
+
+    generatePass(passLength)
+}
+
+fun generatePass(passLength: Int?) {
+
+    val charPool = ('!'..'/') + (0..9) + (' ')
+
+    val password = StringBuilder()
+
+    for (i in passLength!! downTo 1) {
+        val randomChar = charPool.random()
+        password.append(randomChar)
+    }
+    println(password)
+}
