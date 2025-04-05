f2851bf742758f145835faf75acdc3abd4705724
ALEXKIR <yashik.alex2951@mail.ru>
2025-04-05 21:43:10 +0300

    KS-11-1 create class and instances for User

---
+++ src/main/kotlin/lesson11/User.kt
@@ -0,0 +1,10 @@
+package org.example.lesson11
+
+class User(
+    val id: Int,
+    val login: String,
+    val pass: String,
+    val mail: String,
+) {
+
+}
---
+++ src/main/kotlin/lesson11/lesson_11.kt
@@ -0,0 +1,28 @@
+package org.example.lesson11
+
+fun main() {
+
+    val user1 = User(
+        id = 1,
+        login = "user1log",
+        pass = "user1pass",
+        mail = "user1@mail.ru",
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "user2log",
+        pass = "user2pass",
+        mail = "user2@mail.ru",
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.pass)
+    println(user1.mail)
+    println()
+    println(user2.id)
+    println(user2.login)
+    println(user2.pass)
+    println(user2.mail)
+}
