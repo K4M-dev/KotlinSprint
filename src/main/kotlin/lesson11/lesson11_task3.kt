a71106400c67ffb42bb78f9db7a22c468cfdccca
ALEXKIR <yashik.alex2951@mail.ru>
2025-04-12 17:05:17 +0300

    KS-11-3 refactor lines and methods

--- src/main/kotlin/lesson11/lesson11_task3.kt
+++ src/main/kotlin/lesson11/lesson11_task3.kt
@@ -5,9 +5,7 @@ class User(
     var nickName: String,
     var status: String, // "говорит", "микрофон выключен", "пользователь заглушен"
     var userAvatar: String,
-) {
-
-}
+)
 
 class Room(
     var cover: String,
@@ -15,57 +13,17 @@ class Room(
     val members: MutableList<User> = mutableListOf(),
     val userAvatars: MutableList<String> = mutableListOf(),
 ) {
-    fun addMember() {
-        println("Введите id пользователя")
-        val id = readln().toInt()
-        println("Введите никнейм пользователя")
-        val nickName = readln()
-        println("Введите статус пользователя")
-        val status = readln()
-        println("Добавьте аватар для пользователя")
-        val userAvatar = readln()
-        val member = User(id, nickName, status, userAvatar)
+    fun addMember(member: User) {
         members.add(member)
-        userAvatars.add(userAvatar)
         println("Пользователь ${member.nickName} добавлен в комнату $nameRoom")
     }
 
-    fun changeStatus() {
-        println("Введите имя пользователя, кому хотите сменить статус")
-        val nickName = readln()
-        val user = members.find { it.nickName == nickName }
-        if (user != null) {
-            println("Какой статус присвоить пользователю?")
-            user.status = readln()
-            println("Статус пользователя $nickName изменён на ${user.status}")
-        } else println("Такого пользователя нет")
-
+    fun changeStatus(member: User) {
+        println("Пользователь с никнеймом: ${member.nickName} и статусом: ${member.status}")
+        println("Какой статус присвоить пользователю ${member.nickName}?")
+        member.status = readln()
+        println("Статус пользователя ${member.nickName} изменён на ${member.status}")
     }
-
-    fun holdLongTime(member: User) {
-        println("Удерживайте, чтобы подсветить никнейм")
-        val longPress: Boolean = readln().toBoolean()
-        if (longPress) {
-            println(member.nickName)
-        }
-    }
-
-    fun changeCover() {
-        println("Добавьте новое изображение для комнаты")
-        cover = readln()
-        println("Изображение комнаты изменено на $cover")
-    }
-
-    fun changeNameRoom() {
-        println("Введите новое название комнаты")
-        nameRoom = readln()
-        println("Название комнаты изменено на $nameRoom")
-    }
-
-    fun showAllMembers() {
-        println("Все участники: $userAvatars")
-    }
-
 }
 
 fun main() {
@@ -74,12 +32,12 @@ fun main() {
         nameRoom = "Members",
     )
 
-    room1.addMember()
-    room1.changeStatus()
-    room1.changeCover()
-    room1.changeNameRoom()
-    room1.showAllMembers()
-    room1.holdLongTime(room1.members[0])
-
-
+    val user1 = User(
+        id = 1,
+        nickName = "user1",
+        status = "говорит",
+        userAvatar = "picture"
+    )
+    room1.addMember(user1)
+    room1.changeStatus(user1)
 }
