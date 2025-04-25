89f7378b257dd09178e8980d36084883e4141034
ALEXKIR <yashik.alex2951@mail.ru>
2025-04-25 13:19:47 +0300

    KS-13-3 add list for phone directory

---
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -0,0 +1,26 @@
+package org.example.lesson_13
+
+class Directory3(val name: String, val phone: Long, val company: String? = null) {
+
+    fun printInfo() {
+        if (company != null) {
+            println("Имя: $name \nНомер телефона: $phone \nКомпания: $company")
+        } else println("Имя: $name \nНомер телефона: $phone \nКомпания: <не указана>")
+    }
+}
+
+fun main() {
+
+    val directoryList = listOf(
+        Directory3("Igor",89266669212, null),
+        Directory3("Masha",89312332131, null),
+        Directory3("Dima",89773363231, "null"),
+        Directory3("Oleg",89993364320, "Яндекс"),
+        Directory3("Nikita",89331767677, "AGAVA"),
+    )
+
+    for (contact in directoryList) {
+        contact.printInfo()
+        println() // Для раздела
+    }
+}
