49690215cc6e08131246256feb1beb0bf7454415
ALEXKIR <yashik.alex2951@mail.ru>
2025-04-26 12:41:57 +0300

    KS-13-3 fix print companies

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -12,15 +12,17 @@ class Directory3(val name: String, val phone: Long, val company: String? = null)
 fun main() {
 
     val directoryList = listOf(
-        Directory3("Igor",89266669212, null),
-        Directory3("Masha",89312332131, null),
-        Directory3("Dima",89773363231, "null"),
-        Directory3("Oleg",89993364320, "Яндекс"),
-        Directory3("Nikita",89331767677, "AGAVA"),
+        Directory3("Igor", 89266669212, null),
+        Directory3("Masha", 89312332131, null),
+        Directory3("Dima", 89773363231, "null"),
+        Directory3("Oleg", 89993364320, "Яндекс"),
+        Directory3("Nikita", 89331767677, "AGAVA"),
     )
 
     for (contact in directoryList) {
-        contact.printInfo()
-        println() // Для раздела
+        if (contact.company != null) {
+            contact.printInfo()
+            println() // Для раздела
+        }
     }
 }
