4953692bb5f5bd22c8227b779b63452a34581401
ALEXKIR <yashik.alex2951@mail.ru>
2025-05-31 15:58:14 +0300

    KS-17-2 warning to avoid name change

---
+++ src/main/kotlin/lesson_17/lesson17_task2.kt
@@ -0,0 +1,23 @@
+package org.example.lesson_17
+
+class ShipLes17(
+    private val name: String,
+    val avgSpeed: Int,
+    val portRegistry: String,
+) {
+    val _name: String
+        get() = name
+
+    fun changeName(newName: String) {
+        println("Имя корабля менять нельзя. Текущее имя: $_name")
+    }
+}
+
+fun main() {
+
+    val ship1 = ShipLes17("Корабль1", 55, "Порт1")
+
+    println("Имя корабля: ${ship1._name}")
+
+    ship1.changeName("Корабль2")
+}
