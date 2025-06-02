9a32d22d26f6cdea04eb039301894c53088f23ef
ALEXKIR <yashik.alex2951@mail.ru>
2025-06-02 22:21:45 +0300

    KS-18-2 dice game with different sides

---
+++ src/main/kotlin/lesson_18/lesson18_task2.kt
@@ -0,0 +1,65 @@
+package org.example.lesson_18
+
+import kotlin.random.Random
+
+open class GamingDice {
+
+    open fun getDiceResult() {
+        val result: Int = START_DICE
+        println("Кости брошены, выпало число $result")
+    }
+}
+
+class SmallDice(
+    private val edges: Int,
+) : GamingDice() {
+    override fun getDiceResult() {
+        val result = Random.nextInt(FIRST_EDGE, edges)
+        println("Кости с 4-мя гранями брошены, выпало число $result")
+    }
+}
+
+class MediumDice(
+    private val edges: Int,
+) : GamingDice() {
+    override fun getDiceResult() {
+        val result = Random.nextInt(FIRST_EDGE, edges)
+        println("Кости с 6-ю гранями брошены, выпало число $result")
+    }
+}
+
+class BigDice(
+    private val edges: Int,
+) : GamingDice() {
+    override fun getDiceResult() {
+        val result = Random.nextInt(FIRST_EDGE, edges)
+        println("Кости с 8-ю гранями брошены, выпало число $result")
+    }
+}
+
+fun main() {
+
+    val dice4: GamingDice = SmallDice(
+        4
+    )
+
+    val dice6: GamingDice = MediumDice(
+        6
+    )
+
+    val dice8: GamingDice = BigDice(
+        8
+    )
+
+    val listOfDice = listOf<GamingDice>(dice4, dice6, dice8)
+    showAllResults(listOfDice)
+}
+
+fun showAllResults(results: List<GamingDice>) {
+    results.forEach {
+        it.getDiceResult()
+    }
+}
+
+private const val START_DICE = 0
+private const val FIRST_EDGE = 1
