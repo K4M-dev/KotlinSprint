742eb845968411f6ec53f9f3113b6ae1b3e4f147
ALEXKIR <yashik.alex2951@mail.ru>
2025-06-05 23:06:01 +0300

    KS-18-4 create class hierarchy

--- src/main/kotlin/lesson_18/lesson18_task4.kt
+++ src/main/kotlin/lesson_18/lesson18_task4.kt
@@ -2,32 +2,42 @@ package org.example.lesson_18
 
 import kotlin.math.pow
 
-class DateOfPackage {
+open class DateOfPackage {
 
-    fun getAreaOfPackage(
-        length: Int,
-        width: Int,
-        height: Int,
-    ): Int {
-        val area = CONST_FOR_PARALLELEPIPED * (length * height + width * height + length * width)
-        return area
+    open fun getArea(): Int {
+        return START_METHOD
     }
+}
 
-    fun getAreaOfPackage(
-        ribLength: Double,
-    ): Int {
-        val area = CONST_FOR_CUBE * ribLength.pow(2.0).toInt()
-        return area
+class ParallelepipedPackage(
+    private val length: Int,
+    private val width: Int,
+    private val height: Int,
+) : DateOfPackage() {
+
+    override fun getArea(): Int {
+        return CONST_FOR_PARALLELEPIPED * (length * height + width * height + length * width)
+    }
+}
+
+class CubePackage(
+    private val ribLength: Double,
+) : DateOfPackage() {
+
+    override fun getArea(): Int {
+        return CONST_FOR_CUBE * ribLength.pow(2.0).toInt()
     }
 }
 
 fun main() {
 
-    val order = DateOfPackage()
+    val package1 = ParallelepipedPackage(3, 5, 10)
+    val package2 = CubePackage(5.0)
 
-    println(order.getAreaOfPackage(3, 5, 10))
-    println(order.getAreaOfPackage(5.0))
+    println(package1.getArea())
+    println(package2.getArea())
 }
 
 private const val CONST_FOR_PARALLELEPIPED = 2
 private const val CONST_FOR_CUBE = 6
+private const val START_METHOD = 0
