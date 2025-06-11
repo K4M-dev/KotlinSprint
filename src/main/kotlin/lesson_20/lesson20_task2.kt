42fe157a78ac0efa1318af1ea94d13ffa2dbbea8
AlexKir <yashik.alex2951@mail.ru>
2025-06-12 00:20:09 +0300

    KS-20-2 delete method

--- src/main/kotlin/lesson_20/lesson20_task2.kt
+++ src/main/kotlin/lesson_20/lesson20_task2.kt
@@ -3,20 +3,18 @@ package org.example.lesson_20
 class Player20(
     val name: String,
     var currentHP: Int,
-    private val maxHP: Int = 100,
-) {
-    fun useHpFlask(): String {
-        currentHP = maxHP
-        return "Игрок $name, использовал исцелительную колбу и теперь имеет $currentHP hp"
-    }
-}
+    val maxHP: Int = 100,
+)
 
 fun main() {
 
     val player1 = Player20("Guardian", 45)
     println("Текущеее здоровье ${player1.currentHP} hp")
 
-    val hpFlask: (Player20) -> String = { player -> player.useHpFlask() }
+    val hpFlask: (Player20) -> String = { player ->
+        player.currentHP = player.maxHP
+        "Игрок ${player.name}, использовал исцелительную колбу и теперь имеет ${player.currentHP} hp"
+    }
     println(hpFlask(player1))
 
     println("Теперь текущее здоровье: ${player1.currentHP} hp")
