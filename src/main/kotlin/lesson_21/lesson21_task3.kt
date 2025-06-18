4f16a2f7571e0298f96a51b877dfb334f87b7668
ALEXKIR <yashik.alex2951@mail.ru>
2025-06-18 19:55:54 +0300

    KS-21-3 extension fun for player in 20-2

---
+++ src/main/kotlin/lesson_21/lesson21_task3.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_21
+
+class Player21(
+    val name: String,
+    var currentHP: Int,
+    val maxHP: Int = 100,
+)
+
+fun Player21.isHealthy(): Boolean {
+    return currentHP == maxHP
+}
+
+fun main() {
+
+    val player1 = Player21("Guardian", 100)
+    println("Текущее здоровье ${player1.currentHP} hp")
+
+    println(player1.isHealthy())
+
+}
